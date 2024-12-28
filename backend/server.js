293c97ba6f789ee99c854e6a4566bcbd01ff0dee
Thapelo <tapsmcgzee8@gmail.com>
2024-12-28 21:10:31 +0200

    Fixed an issue

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ dotenv.config({ path: '../.env' });
 connectDB();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
