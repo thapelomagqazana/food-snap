dcdd6c07b2606722a534c3cf23f5a88740a3c48e
Thapelo <tapsmcgzee8@gmail.com>
2024-12-28 22:33:02 +0200

    Fixed an issue

--- frontend/food-recognition-app/vite.config.ts
+++ frontend/food-recognition-app/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
   },
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || "https://food-track-api.up.railway.app"),
   },
   resolve: {
     alias: {
